dcf1ec3868155ef360b8ebee1013554a18d569f6
Todd Zebert <todd@toddzebert.com>
2024-11-18 22:00:11 -0800

    improve comments on stars soft PWM/ISR

--- firmware/src/things/stars.c
+++ firmware/src/things/stars.c
@@ -2,6 +2,7 @@
 
 #include <ch32v003fun.h>
 
+// A round-about way of setting PWM_COUNT to 64.
 #define PWM_BIT_DEPTH 6
 #define PWM_COUNT (1 << PWM_BIT_DEPTH)
 
@@ -24,9 +25,20 @@ struct {
 // PD2 is also analog.
 const uint8_t stars_gpio_l_pins[STARS_GPIO_H_PINS_NUM] = {PD4, PD2};
 
-// For the ISR.
+// For the ISR. A static counter var for PWM.
 static int8_t idx = 0;
 
+/**
+ * @brief Handler for the stars thing.
+ *
+ * This function is a handler for the stars thing. It reads the effect ID from
+ * the registry and calls the appropriate effect function, passing the thing ID
+ * and the flag as arguments. During Init it sets up the GPIOs and timers.
+ *
+ * @param event The event struct containing the event type and thing ID.
+ *
+ * @return The return value is the LED "dirty" flag, which is meaningless for Stars.
+ */
 int starsHandler(Event_t event)
 {
     if (!(event.thing == THING_STARS || event.thing == THING_ALL)) return 0;
@@ -36,27 +48,33 @@ int starsHandler(Event_t event)
         // Configure the GPIOs for soft PWM, all high side.
         for (uint8_t i = 0; i < STARS_COUNT; i++)
         {
+            // CFGLR = port configuration register low.
+            // Each pin is 4 bits (CNF & MODE), thus 4 * idx.
+            // The first line resets the bits,
+            // the second line sets them to output mode, 2MHz.
+            // 00: Universal push-pull output mode, is the default setting (after reset).
             Stars_Pin_info[i].gpio->CFGLR &= ~(0xf << (4 * Stars_Pin_info[i].idx));
-            Stars_Pin_info[i].gpio->CFGLR |=  (0x2 << (4 * Stars_Pin_info[i].idx)); // 2MHz push pull
+            Stars_Pin_info[i].gpio->CFGLR |=  (GPIO_Speed_2MHz << (4 * Stars_Pin_info[i].idx));
         }
 
         // Enable GPIOD and TIM2
         RCC->APB1PCENR |= RCC_APB1Periph_TIM2;
         
         // SMCFGR: default clk input is CK_INT
-        // Set TIM2 clock prescaler divider. 
-        TIM2->PSC = 0x0000;
+        TIM2->PSC = 0x0000; // Set TIM2 clock prescaler divider.
         // Set PWM total cycle width.
+        // Automatic Reload Register (ATRLR) reloads the initial value for the CNT at the end of each count cycle.
         TIM2->ATRLR = 2400 - 1;
-        TIM2->CNT = 0;
+        TIM2->CNT = 0; // Reset counter.
 
         // Enable interrupt.
-        TIM2->DMAINTENR |= TIM_UIE;
-        NVIC_SetPriority(TIM2_IRQn, 0 << 6); // @todo meaning?
+        TIM2->DMAINTENR |= TIM_UIE; // DMA/interrupt enable register; Timer Update interrupt enable.
+        // Nested vector interrupt control (NVIC).
+        NVIC_SetPriority(TIM2_IRQn, 0 << 6);
         NVIC_EnableIRQ(TIM2_IRQn);
 
         // Enable TIM2.
-        TIM2->CTLR1 |= TIM_CEN;
+        TIM2->CTLR1 |= TIM_CEN; // Control register 1; Time counter enable.
 
         // These are the Sense (star) LEDs, low side.
         for (int i = 0; i < STARS_GPIO_L_PINS_NUM; i++)
@@ -90,7 +108,7 @@ void TIM2_IRQHandler(void) __attribute__((interrupt));
 void TIM2_IRQHandler(void)
 {
     int8_t i;
-    uint8_t g;
+    uint8_t g; // The resulting PWM value for a given star.
     
     idx--;
 
@@ -110,7 +128,7 @@ void TIM2_IRQHandler(void)
             Stars_Pin_info[i].gpio->BSHR = Stars_Pin_info[i].pin; // BSHR = Port bit set/reset register.
     }
 
-    TIM2->INTFR &= ~TIM_UIF; // Update interrupt flag.
+    TIM2->INTFR &= ~TIM_UIF; // Interrupt Status Register; Update interrupt flag.
 }
 
 
